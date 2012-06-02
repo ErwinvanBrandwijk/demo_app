88f633bb90d4db9ca3b363b6bf222bfcfed6673f
Erwin van Branwdijk <erwinvanbranwdijk@gmail.com>
2012-06-02 13:46:52 +0200

    Following added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 before_filter :guest_user,   :only => [:new, :create]
-before_filter :authenticate, :only => [:edit, :update, :index, :destroy]
+before_filter :authenticate, :except => [:show, :new, :create]
 before_filter :correct_user, :only => [:edit, :update]
 before_filter :admin_user,   :only => :destroy
 
@@ -55,4 +55,18 @@ before_filter :admin_user,   :only => :destroy
     @title = @user.name
   end
   
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,13 @@ DemoApp::Application.routes.draw do
 
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
-  resources :users
+  resources :relationships, :only => [:create, :destroy]
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
